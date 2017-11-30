be22e35a192187887d6d2732f74edef0f1e8cb5c
NathanielMether <nmet3@student.monash.edu>
2017-11-30 11:07:25 +1100

    load quote on stating app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 import StockInfo from './components/StockInfo'
+import { fetchQuoteDorStock } from './api/iex'
+
+fetchQuoteDorStock('nflx')
+  .then((quote) => {
+    console.log('Loaded Netflix', quote)
+  })
 
 class App extends Component {
   state = {
-    quote: {
-      symbol: 'NFLX',
-      companyName: 'Netflix Inc.',
-      primaryExchange: 'Nasdaq Global Select',
-      latestPrice: 188.15,
-      latestSource: 'Close',
-      wek52High: 204.39,
-      week52Low: 113.95
-    }
+    quote: null
+  }
+
+  componentDidMount() {
+    fetchQuoteDorStock('nflx')
+      .then((quote) => {
+        this.setState({ quote: quote})
+      })
   }
 
   render() {
@@ -22,9 +27,15 @@ class App extends Component {
 
     return (
       <div className="App">
-        <StockInfo
-          { ...quote }
-        />
+        {
+          !!quote ? (
+            <StockInfo
+              { ...quote }
+            />
+          ) : (
+            <p>Loading...</p>
+          )
+        }
       </div>
     );
   }
